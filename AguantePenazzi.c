24af65702756b4a14db9eb49c4dfacbffa7ce34c
marcost2 <marcostolcachir@gmail.com>
2021-03-23 18:50:52 -0300

    Esqueleto de funciones en Penazzi.c

--- AguantePenazzi.c
+++ AguantePenazzi.c
@@ -1,5 +1,66 @@
 #include "RomaVictor.h"
+#include <stdio.h>
 
 Grafo ConstruccionDelGrafo(){
+    return NULL;
+}
 
+void DestruccionDelGrafo(Grafo G){
+    return NULL;
+}
+
+Grafo CopiarGrafo(Grafo G){
+    return NULL;
+}
+
+u32 NumeroDeVertices(Grafo G){
+    return NULL;
+}
+
+u32 NumeroDeLados(Grafo G){
+    return NULL;
+}
+
+u32 Delta(Grafo G){
+    return NULL;
+}
+
+u32 Nombre(u32 i, Grafo G){
+    return NULL;
+}
+
+u32 Color(u32 i, Grafo G){
+    return NULL;
+}
+
+u32 Grado(u32 i, Grafo G){
+    return NULL;
+}
+
+u32 ColorVecino(u32 j, u32 i, Grafo G){
+    return NULL;
+}
+
+u32 NombreVecino(u32 j, u32 i, Grafo G){
+    return NULL;
+}
+
+u32 OrdenVecino(u32 j, u32 i, Grafo G){
+    return NULL;
+}
+
+u32 PesoLadoConVecino(u32 j, u32 i, Grafo G){
+    return NULL;
+}
+
+char FijarColor(u32 x, u32 i, Grafo G){
+    return NULL;
+}
+
+char FijarOrden(u32 i, Grafo G, u32 N){
+    return NULL;
+}
+
+u32 FijarPesoLadoConVecino(u32 j, u32 i, u32 p, Grafo G){
+    return NULL;
 }
