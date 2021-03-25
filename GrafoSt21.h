9694c882435da1367d84cdd3a75438aa2480dfed
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-03-25 19:53:00 -0300

    redefinición de la struct grafo

--- GrafoSt21.h
+++ GrafoSt21.h
@@ -4,29 +4,32 @@
 typedef unsigned int u32;
 typedef struct _vertice_t *vertice;
 typedef struct lado_t *vecinos;
+
 struct GrafoSt{
 	u32 n_vertices;
 	u32 m_lados;
     u32 delta; //mayor grado del grafo ∆(G)
-	vertice *vertice_array[];
+	vertice *vertices;
 };
 
+/* 
+    Permite una definicion circular donde
+    cada vecino es un vertice en si mismo.
+*/ 
 struct lado_t {
     u32 peso_u2v;
-    vertice *vecino_u;
+    vertice vertice_j;
 };
 
-struct _vertice_t {
-	u32 nombre_real;
-	u32 etiqueta;
-	u32 grado;
-    u32 color;
-	vecinos *vecinos[];
-};
 /* 
     De esta forma guardamos solo una vez los
     vertices y los accedemos siempre con puntero 
 */
-
+struct _vertice_t {
+	u32 nombre_real;
+	u32 grado; //numero de vecinos
+    u32 color;
+	vecinos *vecinos;
+};
 
 #endif
