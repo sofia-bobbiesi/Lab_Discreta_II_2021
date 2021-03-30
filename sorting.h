14a28fd7dc2888ffb00ec2b95697d5f651c546be
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-03-29 23:16:39 -0300

    algoritmo de ordenamiento

---
+++ sorting.h
@@ -0,0 +1,13 @@
+#ifndef SORTING_H
+#define SORTING_H
+
+#include "RomaVictor.h"
+
+u32 get_max(vertice *a, u32 n);
+
+void radix_sort(Grafo G, u32 n);
+
+void mostrar(vertice *a, u32 n);
+
+
+#endif
