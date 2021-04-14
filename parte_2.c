274a8c6fbdb1437d8bef8ed994801474ae81f155
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-04-14 14:07:58 -0300

    parte_2, Aleatorizar lista

---
+++ parte_2.c
@@ -0,0 +1,57 @@
+#include "RomaVictor.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+u32 Greedy(Grafo G){
+    return ;
+}
+
+char Bipartito(Grafo G){
+    return;
+}
+
+char AleatorizarVertices(Grafo G,u32 R){
+    u32 n_vertx = NumeroDeVertices(G);
+
+    /*
+        Arreglo que se usará para guardar los indices aleatorizados,
+        mediante el algoritmo de Fisher-Yates, para fijar el orden
+        en las respectivas posiciones. 
+    */
+    u32 *orden_aleatorio = calloc(n_vertx, sizeof(u32));
+    if (orden_aleatorio == NULL){
+        return 1;
+    }
+
+    // Inicializa los indices en [0 ... n_vertx-1]
+    for (u32 i = 0u; i < n_vertx; ++i){
+        orden_aleatorio[i] = i;
+    }
+
+    // Establece la semilla que rand() usará para generar números pseudo-aleatorios.
+    srand(R); 
+    /*
+        Algoritmo de Fisher-Yates limitado al R dado. El mismo permite 
+        recorrer toda una selección de forma aleatoria una sola vez.
+    */
+    for (int i = n_vertx-1; i > 0; --i){
+        // Elige un índice aleatorio de 0 a i.
+        int j = rand() % (i+1);
+        // Intercambia arr[i] con el elemento en el índice aleatorio.
+        swap(&orden_aleatorio[i], &orden_aleatorio[j]);
+    }
+
+    // Auxiliar para evaluar si falla FijarOrden y cortar el programa.
+    char check = 0;
+    // Finalmente, se aleatoriza el orden de los vertices del grafo G.
+    for (u32 i = 0u; i < n_vertx; ++i){
+        check = FijarOrden(i,G,orden_aleatorio[i]);
+        if (check == 1) {
+            break;
+        }
+    }
+    free(orden_aleatorio);
+    return check;
+}
