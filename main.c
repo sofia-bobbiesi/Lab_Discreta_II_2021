03d0b11166015bc6dda2900579f953515069f62e
Diegoolei <71725436+Diegoolei@users.noreply.github.com>
2021-04-25 01:48:45 -0300

    Bipartito Finished BB

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main() {
     Grafo graph = ConstruccionDelGrafo();
+    printf("Bipartito: %u (0 si no es bipartito, 1 si lo es)\n", Bipartito(graph));
     DestruccionDelGrafo(graph);
     return 0;
 }
--- parte_2.c
+++ parte_2.c
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/* Intercambia los valores de dos punteros en u32 */
+void swap (u32 *a, u32 *b){
+    u32 temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 // FIXME: al reiniciar el loop se revisan innecesariamente los vecinos varias veces 
 u32 Greedy(Grafo G) {
     u32 n_vertx = NumeroDeVertices(G);
@@ -31,46 +38,102 @@ u32 Greedy(Grafo G) {
     return max_color + 1;
 }
 
-// Devuelve 1 si G es bipartito, 0 si no.
-/* char Bipartito(Grafo G){
-    u32 n_vertx = NumeroDeVertices(G);
+struct cola {
+  u32 n; /* número de elementos de la cola */
+  u32 cant_vertices;
+  struct dato *lista;
+  struct dato *ult;
+};
 
-    for (u32 i = 0; i < n_vertx; i++) {
-        FijarColor(UINT32_MAX, i, G);
-    }
-    for(u32 i = 0; i < n_vertx; ++i){
-        u32 color_actual = i%2; //Solo permite dos colores, 0 y 1
-        if(Color(i,G) == UINT32_MAX){
-            FijarColor(color_actual, i, G);
-        }
-        u32 grado_vertice = Grado(i, G);
-        for(u32 j = 0; j < grado_vertice; ++j){  
-            u32 color_vecino = ColorVecino(j, i, G);
-            if(color_vecino == color_actual){
-                // FIXME
-                Greedy(G);
-                return 0; // No es bipartito
-            }
-            else if(color_vecino == UINT32_MAX){
-                // NOTE: probar si color_actual solito anda
-                color_actual == 0 ? FijarColor(1, j, G) : FijarColor(0, j, G);
-            }
-            // El tercer caso es que el color sea el opuesto al del vertice, por lo que no hago ninguna modificación
-        }
-    }
-   return 1;
-} */
+struct dato
+{
+    u32 orden;
+    struct dato* siguiente;
+};
 
-static void BFS(u32 i, Grafo G){
-    u32 v = i;
-    cola_circular cola = cola_vacia(NumeroDeVertices(G));
-    FijarColor(0, v, G);
-    encolar(cola, v);
+typedef struct cola *Cola;
+typedef struct dato *queue;
+
+Cola crear_cola(u32 cant_vertices){
+    Cola x = (Cola)malloc(sizeof(struct cola));
+    x->n = 0;
+    x->cant_vertices = cant_vertices;
+    x->lista = NULL;
+    x->ult = NULL;
+    return x;
+
+}
+
+// FIXME Reservar memoria por elemento
+// x -> lista = q[0]
+// x -> ult = q[]
+
+void encolar(Cola cola, u32 orden){
+
+    queue q = (queue)malloc(sizeof(struct dato));
+
+    if (cola->lista == NULL){
+        q->orden = orden;
+        q->siguiente = NULL;
+        cola->lista = q;
+        cola->ult = q;
+    }else{
+        q->orden = orden;
+        cola->ult->siguiente = q;
+        cola->ult = cola->ult->siguiente;
+    }
+}
+
+// <- head [2 3 5 -> ] <-
+
+void decolar(Cola cola){
+
+    if(cola->lista != NULL){
+        queue aux = cola->lista;
+        cola->lista = cola->lista->siguiente;
+        free(aux);
+    }
+
+}
+
+void destruir_cola(Cola cola){
+
+    while(cola->lista != NULL){
+        queue aux = cola->lista;
+        cola->lista = cola->lista->siguiente;
+        free(aux);
+    }
+
+    free(cola);
+
+
+}
+
+u32 primero(Cola cola){
+
+    return cola->lista->orden;
+
+}
+
+bool cola_esta_vacia(Cola cola){
+
+    if(cola->lista == NULL)
+        return true;
+
+    return false;
+
+}
+
+static void BFS(Grafo G, u32 fija){
+    u32 v;
+    Cola cola = crear_cola(NumeroDeVertices(G));
+    FijarColor(0, fija, G);
+    encolar(cola, fija);
     while (!cola_esta_vacia(cola)) {
         v = primero(cola);
         decolar(cola);
         for (u32 j = 0; j < Grado(v, G); j++) {
-            if (Color(OrdenVecino(j, v, G), G) == UINT32_MAX) {
+            if (ColorVecino(j, v, G) == UINT32_MAX) { 
                 FijarColor(1 - Color(v, G), OrdenVecino(j, v, G), G);
                 encolar(cola, OrdenVecino(j, v, G));
             }
@@ -79,21 +142,32 @@ static void BFS(u32 i, Grafo G){
     destruir_cola(cola);
 }
 
+bool VerificacionBipartito(Grafo G){
+    for(u32 i = 0; i < NumeroDeVertices(G); i++) {
+        for (u32 j = 0; j < Grado(i, G); j++){
+            if(Color(i, G) == ColorVecino(j, i, G)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 char Bipartito(Grafo G)
 {
     for (u32 i = 0; i < NumeroDeVertices(G); i++) {
 	    FijarColor(UINT32_MAX, i, G);
-    }
+    }    
     for (u32 i = 0; i < NumeroDeVertices(G); i++) {
-        if (Color(i, G) == 2) {
-            BFS(i, G); // DFS sería mejor?
+        if (Color(i, G) == UINT32_MAX) {
+            BFS(G,i);
         }
     }
-    if (!ChequeoColoreo(G)) {
+    if (!VerificacionBipartito(G)) {
         for (u32 i = 0; i < NumeroDeVertices(G); i++) {
             FijarColor(i, i, G);
         }
+        
 	    return 0;
     }
     return 1;
