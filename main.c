444d67e7d3aacc53029e7505b5fd2316eb24c74e
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-05-19 19:51:27 -0300

    modularizar main + arreglar acceso invalido

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "UnleashHell.h"
+#include "auxiliares.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -17,17 +18,23 @@ void RandomizarPermutaciones(u32* perm, u32 len, u32 R){
     srand(R); 
     // Algoritmo de Fisher-Yates limitado al R dado.
    int j;
-    for (u32 i = len-1; i > 0; --i){
+    for (u32 i = len - 1; i > 0; --i){
         // Elige un índice aleatorio de 0 a i.
         j = rand() % (i+1);
         swap(&perm[i], &perm[j]);
     }
 }
 
+void MayorMenor(u32* perm, u32 len){
+    for (u32 k = 0; k < len; ++k){
+        perm[k] = len-k-1;
+    }
+}
+
 int main(int argc, char *argv[]) {
     // Inciso 1
     if(argc != 7){
-        fprintf(stderr,BRED "ERROR: Formato inválido\n");
+        fprintf(stderr, BRED "ERROR: Formato inválido\n");
         fprintf(stderr,"Cerrando el programa...\n"reset);
         exit(1);
     }
@@ -40,7 +47,7 @@ int main(int argc, char *argv[]) {
 
     Grafo G = ConstruccionDelGrafo();
     if(G == NULL){
-        fprintf(stderr,BRED "ERROR: No se pudo cargar el grafo\n");
+        fprintf(stderr, BRED "ERROR: No se pudo cargar el grafo\n");
         fprintf(stderr,"Cerrando el programa...\n"reset);
         exit(1);
     }
@@ -48,12 +55,12 @@ int main(int argc, char *argv[]) {
     // Inciso 2
     printf(BMAG "Número de vértices para G: %u\n", NumeroDeVertices(G));
     printf("Número de lados para G: %u\n", NumeroDeLados(G));
-    printf("Delta de G: %u\n\n",Delta(G));
+    printf("Delta de G: %u\n\n", Delta(G));
 
     // Inciso 3
     if(Bipartito(G)){
-        fprintf(stderr,BGRN "El grafo G es bipartito\n");
-        fprintf(stderr,BRED "Cerrando el programa...\n"reset);
+        fprintf(stderr, BGRN "El grafo G es bipartito\n");
+        fprintf(stderr, BRED "Cerrando el programa...\n"reset);
         DestruccionDelGrafo(G);
         exit(0);
     }
@@ -64,21 +71,21 @@ int main(int argc, char *argv[]) {
     OrdenNatural(G);
     u32 color = Greedy(G);
     u32 mejor_color_G = color;
-    printf(BBLU "El coloreo fue de %u colores para el orden natural\n\n"reset,color);
+    printf(BBLU "El coloreo fue de %u colores para el orden natural\n\n"reset, color);
     int mejor_semilla = f;
     for (int i = 0; i < a; ++i) {
-        AleatorizarVertices(G,f+i);
+        AleatorizarVertices(G, f+i);
         color = Greedy(G);
-        printf("El coloreo fue de %u por aleatorización de colores\n",color);
+        printf("El coloreo fue de %u por aleatorización de colores\n", color);
         if(color < mejor_color_G){
             mejor_color_G = color;
-            mejor_semilla = f+i;
+            mejor_semilla = f + i;
         }
     }
-    superGreedyto += a+1;
+    superGreedyto += a + 1;
     // Si la mejor semilla no fue la última, vuelvo a ordenar
-    if(mejor_semilla != f+a-1) {
-        AleatorizarVertices(G,mejor_semilla);
+    if(mejor_semilla != (f + a - 1)) {
+        AleatorizarVertices(G, mejor_semilla);
         mejor_color_G = Greedy(G);
         superGreedyto++;
     }
@@ -87,8 +94,8 @@ int main(int argc, char *argv[]) {
     // Inciso 6
     u32 *perm = calloc(mejor_color_G, sizeof(u32));
     for (int i = 0; i < b; ++i) {
-        RandomizarPermutaciones(perm,mejor_color_G,f+i);
-        OrdenPorBloqueDeColores(G,perm);
+        RandomizarPermutaciones(perm, mejor_color_G, f+i);
+        OrdenPorBloqueDeColores(G, perm);
         mejor_color_G = Greedy(G);
         printf("El coloreo fue de %u por bloques de colores\n", mejor_color_G);
     }
@@ -106,35 +113,29 @@ int main(int argc, char *argv[]) {
         Grafo W = CopiarGrafo(G);
         for (int j = 0; j < d; ++j) {
             // Estrategia A:
-            RandomizarPermutaciones(perm,mejor_color_G,f+j);
-            OrdenPorBloqueDeColores(G,perm);
+            RandomizarPermutaciones(perm, mejor_color_G, f+j);
+            OrdenPorBloqueDeColores(G, perm);
             mejor_color_G = Greedy(G);     
 
             // Estrategia B:
-            for (u32 k = 0; k < mejor_color_H; ++k){
-                perm[k] = mejor_color_H-k-1;
-            }
-            OrdenPorBloqueDeColores(H,perm);
+            MayorMenor(perm,mejor_color_H);
+            OrdenPorBloqueDeColores(H, perm);
             mejor_color_H = Greedy(H);
 
             // Estrategia C:
             u32 idx_rand = 0;
-            u32 rand_fijo = rand() % e;
 
             if (mejor_color_H != mejor_color_W) {
-                for (u32 k = 0; k < mejor_color_W; ++k){
-                    perm[k] = mejor_color_H-k-1;
-                }
+                MayorMenor(perm,mejor_color_H);
             }
-            
+
             for (u32 k = 0; k < mejor_color_W; ++k) {
                 idx_rand = rand() % e;
-                if (idx_rand == rand_fijo) {
-                    idx_rand = rand() % (mejor_color_W+1);
-                    swap(&perm[k],&perm[idx_rand]);
+                if (idx_rand == 0) {
+                    swap(&perm[k], &perm[rand() % mejor_color_W]);
                 }
             }
-            OrdenPorBloqueDeColores(W,perm);
+            OrdenPorBloqueDeColores(W, perm);
             mejor_color_W = Greedy(W);
             
             printf("El coloreo obtenido fue de G: %u, H: %u, W: %u colores\n", mejor_color_G, mejor_color_H, mejor_color_W);
