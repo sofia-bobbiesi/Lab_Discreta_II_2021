4ed12d57280bf41642256e5b359dc983e74bf473
Sofia Bobbiesi <sofia.bobbiesi@mi.unc.edu.ar>
2021-05-22 22:03:02 -0300

    mejor especificacion

--- UnleashHell.h
+++ UnleashHell.h
@@ -7,29 +7,31 @@
 /* 
     Corre greedy en G comenzando con el color 0, con el orden 
     interno que debe estar guardado de alguna formadentro de G. 
-    Devuelve el número de colores que se obtiene.
+    Devuelve el número de colores que se obtiene. Devuelve 2**32-1 
+    si hubo algún error interno y no pudo continuar el coloreo.
 */
 u32 Greedy(Grafo G);
 
 /*
-    Devuelve 1 si G es bipartito, 0 si no. Ademas, si devuelve 1, colorea
-    G con un coloreo propio de dos colores.  Si devuelve 0, debe dejar a G 
+    Devuelve 1 si G es bipartito, 0 si no. Además, si devuelve 1, colorea
+    G con un coloreo propio de dos colores. Si devuelve 0, deja a G 
     coloreado con algun coloreo propio. (no necesariamente el mismo que
     tenia al principio).
 */
 char Bipartito(Grafo G);
 
 /*
-    “Aleatoriza” el orden de los v ́ertices de G, usando como semilla de
+    “Aleatoriza” el orden de los vértices de G, usando como semilla de
     aleatoridad el numero R. Retorna 0 si todo anduvo bien y 1 si hubo
     algun problema.
 */
 char AleatorizarVertices(Grafo G,u32 R);
 
-
 /*
     Ordena los vertices por colores, poniendo los vertices 
     de color perm[0] primero, luego los de color perm[1], etc...
+    Si perm NO es una permutación devuelve 0, de lo contrario, 
+    ordena y devuelve 1.
 */
 char OrdenPorBloqueDeColores(Grafo G,u32* perm);
 
